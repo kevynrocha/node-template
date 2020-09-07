3c427e98d368f838bc4a066792b5d91661fb742f
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 03:29:58 -0400

    criado middleware de autenticação e melhorando rotas

---
+++ src/middlewares/verifyAuthentication.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+const verifyAuthentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response | void => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new Error('JWT não foi enviado');
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+      const decoded = verify(token, authConfig.jwt.secret);
+
+      const { sub } = decoded as TokenPayload;
+
+      req.user = {
+        id: sub,
+      };
+    } catch {
+      throw new Error('JWT com assinatura inválida');
+    }
+
+    return next();
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
+
+export default verifyAuthentication;
