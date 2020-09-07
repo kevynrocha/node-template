a821542d9fdbf4646ec59f38a58eb8f4988e7505
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 03:30:17 -0400

    criado server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) =>
-  response.json({
-    message: 'Meu server Express, Typescript e ESLint!',
-  }),
-);
-
-app.listen(3333, () => {
-  console.log('Back-end started in 3333 port!');
-});
+app
+  .listen(3333, () => {
+    console.log('Back-end started in 3333 port!');
+  })
+  .on('error', e => console.log(`Back-end don't started`, e));
