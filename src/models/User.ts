80fb12abb217ff6405824c39f1037c1972ae9fc4
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 03:27:38 -0400

    criado model de usuários

---
+++ src/models/User.ts
@@ -0,0 +1,43 @@
+import { DataTypes, UUIDV4, Model, Optional } from 'sequelize';
+import connection from '../database/connection';
+
+export interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+type UserCreationAttributes = Optional<UserAttributes, 'id'>;
+
+interface UserInstance
+  extends Model<UserAttributes, UserCreationAttributes>,
+    UserAttributes {}
+
+const UserModel = connection.define<UserInstance>('users', {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    allowNull: false,
+    defaultValue: UUIDV4,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+connection.sync({ force: true });
+
+export default UserModel;
