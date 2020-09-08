7f8e0597766a2c6ad3835aeb81bcd1eb225d3064
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 20:36:15 -0400

    port no .env

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
+import './config/env';
 import express from 'express';
 import routes from './routes';
+
+const { port } = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
 app
-  .listen(3333, () => {
-    console.log('Back-end started in 3333 port!');
+  .listen(port || 3333, () => {
+    console.log(`Back-end started in ${port || 3333} port!`);
   })
   .on('error', e => console.log(`Back-end don't started`, e));
