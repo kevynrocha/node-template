adfd184f76057a24ff2dbf655330389c45b9df13
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 22:07:50 -0400

    adicionado porta no server

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import './config/env';
 import express from 'express';
 import routes from './routes';
 
-const { port } = process.env;
+const port = process.env.PORT || 3333;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
 app
-  .listen(port || 3333, () => {
+  .listen(port, () => {
     console.log(`Back-end started in ${port || 3333} port!`);
   })
   .on('error', e => console.log(`Back-end don't started`, e));
