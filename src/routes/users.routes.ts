eefdf35c6a98e8e4c110cb7573e87506e179ff0a
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 23:06:23 -0400

    validações do usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+// import verifyAuthentication from '../middlewares/verifyAuthentication';
 import UserController from '../controllers/UserController';
+import { validate } from '../middlewares/validateRequest';
+import {
+  showAndUpdateValidationRules,
+  storeValidationRules,
+} from '../validations/users.validations';
 
 const router = Router();
 
 router.get('/', UserController.index);
-router.post('/', UserController.store);
+router.get('/:id', showAndUpdateValidationRules, validate, UserController.show);
+router.post('/', storeValidationRules, validate, UserController.store);
+router.put('/:id', UserController.update);
+router.delete(
+  '/:id',
+  showAndUpdateValidationRules,
+  validate,
+  UserController.destroy,
+);
 
 export default router;
