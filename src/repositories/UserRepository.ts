717a9756015a6b4a0512e79b7846a5e2e35458e2
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-07 22:45:10 -0400

    Adicionado transações e novos métodos

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,6 +1,8 @@
 import User, { UserAttributes } from '../models/User';
+import connection from '../database/connection';
 
 interface Request {
+  id?: string;
   name: string;
   email: string;
   password: string;
@@ -11,8 +13,20 @@ const create = async ({
   email,
   password,
 }: Request): Promise<UserAttributes> => {
-  const user = await User.create({ name, email, password });
-  return user;
+  const t = await connection.transaction();
+
+  try {
+    const user = await User.create(
+      { name, email, password },
+      { transaction: t },
+    );
+
+    await t.commit();
+    return user;
+  } catch (error) {
+    await t.rollback();
+    return error.message;
+  }
 };
 
 const findAll = async (): Promise<UserAttributes[]> => {
@@ -20,7 +34,70 @@ const findAll = async (): Promise<UserAttributes[]> => {
   return users;
 };
 
+const findOneById = async (id: string): Promise<UserAttributes | null> => {
+  const user = await User.findOne({
+    where: {
+      id,
+    },
+  });
+  return user;
+};
+
+const findOneByEmail = async (
+  email: string,
+): Promise<UserAttributes | null> => {
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+  return user;
+};
+
+const update = async ({
+  id,
+  name,
+  email,
+  password,
+}: Request): Promise<Array<number | UserAttributes[]>> => {
+  const t = await connection.transaction();
+
+  try {
+    const user = await User.update(
+      { name, email, password },
+      { where: { id }, transaction: t },
+    );
+    await t.commit();
+    return user;
+  } catch (error) {
+    await t.rollback();
+    return error.message;
+  }
+};
+
+const destroy = async (id: string): Promise<number> => {
+  const t = await connection.transaction();
+
+  try {
+    const user = await User.destroy({
+      where: {
+        id,
+      },
+      transaction: t,
+    });
+    await t.commit();
+    return user;
+  } catch (error) {
+    await t.rollback();
+    return error.message;
+  }
+};
+
 export default {
   findAll,
   create,
+  findOneById,
+  findOneByEmail,
+  update,
+  destroy,
 };
