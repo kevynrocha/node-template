c807e19e24e6bc1cabfeaca9150a95afb01b4f1a
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-09 20:54:14 -0400

    Criando middleware para capturar erros

---
+++ src/middlewares/handleResponseErrors.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from 'src/logger';
+
+const handleResponseErrors = (
+  error: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response => {
+  logger.error(error.message);
+  return res.status(500).json({
+    name: error.name,
+    message: error.message,
+  });
+};
+
+export default handleResponseErrors;
