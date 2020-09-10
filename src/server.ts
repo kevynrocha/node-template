06a85907927bd8e5ad350d812b3b78656f2ceea8
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-09 21:22:29 -0400

    adicionado novos middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import '@config/env';
+import 'express-async-errors';
 import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
 import logger from './logger';
 import routes from './routes';
+import handleResponseErrors from './middlewares/handleResponseErrors';
 
 const port = process.env.PORT || 3333;
 
 const app = express();
-
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(routes);
+app.use(handleResponseErrors);
 
 app
   .listen(port, () => {
