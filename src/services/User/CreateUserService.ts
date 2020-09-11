160bfa241eccaca7330ea74cb0e64ab7073f3adb
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-11 13:06:47 -0400

    criado camada de serviços

---
+++ src/services/User/CreateUserService.ts
@@ -0,0 +1,34 @@
+import UserRepository from '@repositories/UserRepository';
+import { IUserAttributes } from '@models/User';
+import AppError from '@src/errors/AppError';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const CreateUserService = async ({
+  name,
+  email,
+  password,
+}: IRequest): Promise<IUserAttributes> => {
+  const checkUserExists = await UserRepository.findOneByEmail(email);
+
+  if (checkUserExists) {
+    throw AppError('Email já existe', 401);
+  }
+
+  const hashedPassword = await hash(password, 8);
+
+  const user = await UserRepository.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  return user;
+};
+
+export default CreateUserService;
