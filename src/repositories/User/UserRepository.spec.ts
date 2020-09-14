d22a78a24c7112ba961082e16b700d0ee1d8dec6
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-09-13 22:10:55 -0400

    tests users - repository, service

--- src/repositories/User/UserRepository.spec.ts
+++ src/repositories/User/UserRepository.spec.ts
@@ -1,42 +1,43 @@
 import '@config/env';
 import connection from '@src/database/connection';
+import User from '@src/models/User';
 import UserRepository from './UserRepository';
 
 beforeEach(async () => {
-  await connection.sync({ force: true, logging: false });
+  await User.destroy({ truncate: true });
 });
 
 afterAll(async () => {
+  await User.destroy({ truncate: true });
   await connection.close();
 });
 
 describe('User Repository', () => {
   it('should be able to create a new user', async () => {
     const user = await UserRepository.create({
-      name: 'name',
-      email: 'email@email.com',
-      password: '123456',
+      name: 'Name Repository',
+      email: 'email_repository@email.com',
+      password: 'repository',
     });
-
-    expect(user?.name).toEqual('name');
-    expect(user?.email).toEqual('email@email.com');
-    expect(user?.password).toEqual('123456');
+    expect(user?.name).toEqual('Name Repository');
+    expect(user?.email).toEqual('email_repository@email.com');
+    expect(user?.password).toEqual('repository');
     expect(user?.id).toHaveLength(36);
     expect(user).toHaveProperty('id');
   });
 
   it('should not be able to create a new user with same e-mail', async () => {
     await UserRepository.create({
-      name: 'name',
-      email: 'email@email.com',
-      password: '123456',
+      name: 'Name Repository',
+      email: 'email_repository@email.com',
+      password: 'repository',
     });
 
     expect(
       UserRepository.create({
-        name: 'name',
-        email: 'email@email.com',
-        password: '123456',
+        name: 'Name Repository',
+        email: 'email_repository@email.com',
+        password: 'repository',
       }),
     ).rejects.toBeInstanceOf(Error);
   });
--- src/services/User/CreateUserService.spec.ts
+++ src/services/User/CreateUserService.spec.ts
@@ -1,19 +1,42 @@
 import '@config/env';
 import connection from '@src/database/connection';
+import AppError from '@src/errors/AppError';
+import User from '@src/models/User';
 import CreateUserService from './CreateUserService';
 
-beforeAll(async () => {
-  await connection.sync({ force: true });
+beforeEach(async () => {
+  await User.destroy({ truncate: true });
 });
 
-describe('UserController', () => {
+afterAll(async () => {
+  await User.destroy({ truncate: true });
+  await connection.close();
+});
+
+describe('User Service', () => {
   it('should be able to create a new user', async () => {
     const user = await CreateUserService({
-      name: 'Kevyn Test',
-      email: 'Kevyn email',
-      password: 'Kevyn password',
+      name: 'Name Service',
+      email: 'email_service@email.com',
+      password: 'Service',
     });
 
-    await expect(user).toBeTruthy();
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same e-mail', async () => {
+    await CreateUserService({
+      name: 'Name Service',
+      email: 'email_service@email.com',
+      password: 'Service',
+    });
+
+    expect(
+      CreateUserService({
+        name: 'Name Service',
+        email: 'email_service@email.com',
+        password: 'Service',
+      }),
+    ).rejects.toEqual(AppError('Email já existe', 401));
   });
 });
